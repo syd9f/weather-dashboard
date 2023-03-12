ada5546e18dde8f45c886c8fd76ae0c88a8a0791
sydnief <sydniefarrell@gmail.com>
2023-03-12 17:36:09 -0400

    began coding script/cityinput value not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,39 @@
-console.log()
+var searchButton = document.getElementById("searchBtn");
+var cityInput = document.getElementById("cityInput").value;
+
+searchButton.addEventListener("click", storeCitySearch);
+console.log(cityInput);
+
+function storeCitySearch() {
+    localStorage.setItem("city", cityInput);
+    getCityCoordinates;
+}
+
+function getCityCoordinates() {
+    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='+ cityInput + '&appid=585f64e0a63a1964d8bafef222a8e541';
+
+    fetch(requestUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then (function(data) {
+        console.log(cityInput)
+        getFiveDayForecast;
+    });
+}
+
+
+function getFiveDayForecast() {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=585f64e0a63a1964d8bafef222a8e541';
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(cityInput)
+    });
+
+
+
+}
