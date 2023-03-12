6242e9346472cce467abb0c96087ae9499c7a7c3
sydnief <sydniefarrell@gmail.com>
2023-03-12 19:47:51 -0400

    logged weather data for five day forecast to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,9 @@ searchButton.addEventListener("click", storeCitySearch);
 function storeCitySearch() {
     var cityInput = document.getElementById("cityInput").value;
     localStorage.setItem("citySearchHistory", cityInput);
+
     console.log(cityInput);
+
     getCityCoordinates();
 }
 
@@ -22,23 +24,33 @@ function getCityCoordinates() {
     //    get lat and lon from data
         var cityLat = data[0].lat;
         var cityLon = data[0].lon;
+
         console.log(cityLat);
         console.log(cityLon);
-        // getFiveDayForecast();
+
+        var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&appid=585f64e0a63a1964d8bafef222a8e541';
+        getFiveDayForecast(requestUrl);
         
     });
 }
 
 
-function getFiveDayForecast() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&appid=585f64e0a63a1964d8bafef222a8e541';
-
+function getFiveDayForecast(requestUrl) {
+    
     fetch(requestUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function(data) {
-        
+        console.log(data);
+
+        for (var i = 0; i < 5; i++) {
+            console.log(data.list[i].main.temp);
+            console.log(data.list[i].wind.speed);
+            console.log(data.list[i].main.humidity);
+
+          //   append results to page
+        }
     });
 
 }
