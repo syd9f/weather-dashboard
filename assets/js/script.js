aaa2351862010d1f3374a7f025c8555f9f7cb599
sydnief <sydniefarrell@gmail.com>
2023-03-12 19:35:18 -0400

    fixed request url, can now get lat and lon of city

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,33 +2,36 @@
 var searchButton = document.getElementById("searchBtn");
 searchButton.addEventListener("click", storeCitySearch);
 
-
 // Gets user input and saves to local storage, calls next function
 function storeCitySearch() {
     var cityInput = document.getElementById("cityInput").value;
-    localStorage.setItem("city", cityInput);
+    localStorage.setItem("citySearchHistory", cityInput);
     console.log(cityInput);
     getCityCoordinates();
 }
 
 function getCityCoordinates() {
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='+ cityInput + '&appid=585f64e0a63a1964d8bafef222a8e541';
+    var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + localStorage.getItem("citySearchHistory") + '&appid=585f64e0a63a1964d8bafef222a8e541';
 
     fetch(requestUrl)
     .then(function(response) {
         return response.json();
     })
     .then (function(data) {
+        console.log(data);
     //    get lat and lon from data
-
-        getFiveDayForecast();
+        var cityLat = data[0].lat;
+        var cityLon = data[0].lon;
+        console.log(cityLat);
+        console.log(cityLon);
+        // getFiveDayForecast();
         
     });
 }
 
 
 function getFiveDayForecast() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=585f64e0a63a1964d8bafef222a8e541';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&appid=585f64e0a63a1964d8bafef222a8e541';
 
     fetch(requestUrl)
     .then(function (response) {
@@ -37,7 +40,5 @@ function getFiveDayForecast() {
     .then(function(data) {
         
     });
-
-
 
 }
