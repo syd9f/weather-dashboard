cdd4d0ac928a9265da1b6a09bfea6ae30e34b3e4
sydnief <sydniefarrell@gmail.com>
2023-03-19 18:42:10 -0400

    created function for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var searchButton = document.getElementById("searchBtn");
 searchButton.addEventListener("click", storeCitySearch);
 
-// Gets user input and saves to local storage, calls next function
+// Gets user input and saves to local storage, calls next function to get coordinates
 function storeCitySearch() {
     var cityInput = document.getElementById("cityInput").value;
     localStorage.setItem("citySearchHistory", cityInput);
@@ -12,7 +12,7 @@ function storeCitySearch() {
     getCityCoordinates();
 }
 
-// takes user input and gets city coordinates for city, calls functions to display weather data
+// takes user input and gets city coordinates for city, calls next functions to display weather data
 function getCityCoordinates() {
     var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + localStorage.getItem("citySearchHistory") + '&appid=585f64e0a63a1964d8bafef222a8e541';
 
@@ -30,16 +30,32 @@ function getCityCoordinates() {
         console.log(cityLat);
         console.log(cityLon);
 
-        var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&appid=585f64e0a63a1964d8bafef222a8e541&units=imperial';
-        getFiveDayForecast(requestUrl);
-        
+        var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&appid=585f64e0a63a1964d8bafef222a8e541&units=imperial';
+        getFiveDayForecast(forecastUrl);
+
+        var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + cityLat + '&lon='  + cityLon + '&appid=585f64e0a63a1964d8bafef222a8e541&units=imperial';
+        getCurrentWeather(currentUrl);
     });
 }
 
+// fetches current weather data for selected city and displays to page
+function getCurrentWeather(currentUrl) {
+    fetch(currentUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data.weather);
+        console.log('Temp today: ' + data.main.temp);
+        console.log('Humidity today: ' + data.main.humidity);
+        console.log('Wind Speed today: ' + data.wind.speed);
+    })
+}
 
-function getFiveDayForecast(requestUrl) {
+// fetches five day forecast for selected city and displays to page
+function getFiveDayForecast(forecastUrl) {
     
-    fetch(requestUrl)
+    fetch(forecastUrl)
     .then(function (response) {
         return response.json();
     })
@@ -49,60 +65,60 @@ function getFiveDayForecast(requestUrl) {
         var cityName = document.getElementById('cityName');
         cityName.innerHTML = data.city.name + ', ' + data.city.country;
 
-        var fiveDayEl = document.getElementById('5-day');
+        // selects page elements
         var day1 = document.getElementById('day1');
         var day2 = document.getElementById('day2');
         var day3 = document.getElementById('day3');
         var day4 = document.getElementById('day4');
         var day5 = document.getElementById('day5');
 
+        // loops through api results in console
         for (var i = 0; i < 5; i++) {
             console.log("Day " + (i+1) + ' Temp: ' + data.list[i].main.temp);
             console.log("Day " + (i+1) + ' Wind: ' + data.list[i].wind.speed);
             console.log("Day " + (i+1) + ' Humidity: ' + data.list[i].main.humidity);
             console.log("Day " + (i+1) + " Weather: " + JSON.stringify(data.list[i].weather));
-        
-          //   append results to page
-            day1.innerHTML =   '<div class="card text-bg-success col-2 forecast" style="width:20%">' + 
-            '<div class="card-body">' +
-                '<h5 class="card-title">' + 'Day 1: ' + '</h5>' +
-                '<p class="card-text">' + 'Temp: ' + data.list[0].main.temp + '</p>' +
-                '<p class="card-text">' + 'Wind: ' + data.list[0].wind.speed + '</p>' +
-                '<p class="card-text">' + 'Humidity: ' + data.list[0].main.humidity + '</p>' +
-            '</div>' + '</div>';
-          day2.innerHTML = 
-          '<div class="card text-bg-success col-2 forecast" style="width:20%">' + 
-          '<div class="card-body">' +
-              '<h5 class="card-title">' + 'Day 2: ' + '</h5>' +
-              '<p class="card-text">' + 'Temp: ' + data.list[1].main.temp + '</p>' +
-              '<p class="card-text">' + 'Wind: ' + data.list[1].wind.speed + '</p>' +
-              '<p class="card-text">' + 'Humidity: ' + data.list[1].main.humidity + '</p>' +
-          '</div>' + '</div>';
-          day3.innerHTML = 
-          '<div class="card text-bg-success col-2 forecast " style="width:20%">' + 
-          '<div class="card-body">' +
-              '<h5 class="card-title">' + 'Day 3: ' + '</h5>' +
-              '<p class="card-text">' + 'Temp: ' + data.list[2].main.temp + '</p>' +
-              '<p class="card-text">' + 'Wind: ' + data.list[2].wind.speed + '</p>' +
-              '<p class="card-text">' + 'Humidity: ' + data.list[2].main.humidity + '</p>' +
-          '</div>' + '</div>';
-          day4.innerHTML = 
-          '<div class="card text-bg-success col-2 forecast" style="width:20%">' + 
-          '<div class="card-body">' +
-              '<h5 class="card-title">' + 'Day 4: ' + '</h5>' +
-              '<p class="card-text">' + 'Temp: ' + data.list[3].main.temp + '</p>' +
-              '<p class="card-text">' + 'Wind: ' + data.list[3].wind.speed + '</p>' +
-              '<p class="card-text">' + 'Humidity: ' + data.list[3].main.humidity + '</p>' +
-          '</div>' + '</div>';
-          day5.innerHTML = 
-          '<div class="card text-bg-success col-2 forecast" style="width:20%">' + 
-          '<div class="card-body">' +
-              '<h5 class="card-title">' + 'Day 5: ' + '</h5>' +
-              '<p class="card-text">' + 'Temp: ' + data.list[4].main.temp + '</p>' +
-              '<p class="card-text">' + 'Wind: ' + data.list[4].wind.speed + '</p>' +
-              '<p class="card-text">' + 'Humidity: ' + data.list[4].main.humidity + '</p>' +
-          '</div>' + '</div>';
         }
+        //   append results to page
+        day1.innerHTML =   '<div class="card text-bg-success col-2 forecast" style="width:20%">' + 
+        '<div class="card-body">' +
+            '<h5 class="card-title">' + 'Day 1: ' + '</h5>' +
+            '<p class="card-text">' + 'Temp: ' + data.list[0].main.temp + '</p>' +
+            '<p class="card-text">' + 'Wind: ' + data.list[0].wind.speed + '</p>' +
+            '<p class="card-text">' + 'Humidity: ' + data.list[0].main.humidity + '</p>' +
+        '</div>' + '</div>';
+        day2.innerHTML = 
+        '<div class="card text-bg-success col-2 forecast" style="width:20%">' + 
+        '<div class="card-body">' +
+            '<h5 class="card-title">' + 'Day 2: ' + '</h5>' +
+            '<p class="card-text">' + 'Temp: ' + data.list[1].main.temp + '</p>' +
+            '<p class="card-text">' + 'Wind: ' + data.list[1].wind.speed + '</p>' +
+            '<p class="card-text">' + 'Humidity: ' + data.list[1].main.humidity + '</p>' +
+        '</div>' + '</div>';
+        day3.innerHTML = 
+        '<div class="card text-bg-success col-2 forecast " style="width:20%">' + 
+        '<div class="card-body">' +
+            '<h5 class="card-title">' + 'Day 3: ' + '</h5>' +
+            '<p class="card-text">' + 'Temp: ' + data.list[2].main.temp + '</p>' +
+            '<p class="card-text">' + 'Wind: ' + data.list[2].wind.speed + '</p>' +
+            '<p class="card-text">' + 'Humidity: ' + data.list[2].main.humidity + '</p>' +
+        '</div>' + '</div>';
+        day4.innerHTML = 
+        '<div class="card text-bg-success col-2 forecast" style="width:20%">' + 
+        '<div class="card-body">' +
+            '<h5 class="card-title">' + 'Day 4: ' + '</h5>' +
+            '<p class="card-text">' + 'Temp: ' + data.list[3].main.temp + '</p>' +
+            '<p class="card-text">' + 'Wind: ' + data.list[3].wind.speed + '</p>' +
+            '<p class="card-text">' + 'Humidity: ' + data.list[3].main.humidity + '</p>' +
+        '</div>' + '</div>';
+        day5.innerHTML = 
+        '<div class="card text-bg-success col-2 forecast" style="width:20%">' + 
+        '<div class="card-body">' +
+            '<h5 class="card-title">' + 'Day 5: ' + '</h5>' +
+            '<p class="card-text">' + 'Temp: ' + data.list[4].main.temp + '</p>' +
+            '<p class="card-text">' + 'Wind: ' + data.list[4].wind.speed + '</p>' +
+            '<p class="card-text">' + 'Humidity: ' + data.list[4].main.humidity + '</p>' +
+        '</div>' + '</div>';
     });
 
 }
